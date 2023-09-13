8223af23b0b51cd9646683a65e4f6380e7031bd4
Ayatullah Khalid <ayatullahkhalid1110@gmail.com>
2023-09-13 12:52:27 +0100

    solved 7th challenge

--- 7. arrayReduceTransformation.js
+++ 7. arrayReduceTransformation.js
@@ -3,20 +3,10 @@
  * @param {Function} fn
  * @param {number} init
  * @return {number}
- **/
+ */
 var reduce = function (nums, fn, init) {
-  let answer = 0;
-  for (let i = 0; i < nums.length; i++) {
-    answer = fn(init, nums[i]);
-    init= answer;
-  }
-  return answer
+    for (let i = 0; i < nums.length; i++) {
+        init = fn(init, nums[i]);
+    }
+    return init;
 };
-
-nums = [1, 2, 3, 4];
-fn = function sum(accum, curr) {
-  return accum + (curr * curr);
-};
-init = 100;
-reduce(nums, fn, init);
-//console.log(reduce(nums, fn, init));
