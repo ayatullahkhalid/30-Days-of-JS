23c119d42dea9adaa13a27dc7ec68668399778c8
Ayatullah Khalid <ayatullahkhalid1110@gmail.com>
2023-09-15 23:11:52 +0100

    addedd links for the 10th challenge

--- 5. applyTransformOverEachElementInArray.js
+++ 5. applyTransformOverEachElementInArray.js
@@ -3,10 +3,10 @@
  * @param {Function} fn
  * @return {number[]}
  */
-var map = function(arr, fn) {
-    let array = []
-    for(let i= 0; i<arr.length;i++){
-        array.push(fn(arr[i],i));
-        }
-    return array;
+var map = function (arr, fn) {
+  let array = [];
+  for (let i = 0; i < arr.length; i++) {
+    array.push(fn(arr[i], i));
+  }
+  return array;
 };
